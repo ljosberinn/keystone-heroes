36e2a4fb63f7543b600d78a1a77763e9a1e27f73
Gerrit Alex <admin@gerritalex.de>
2021-05-13 22:37:17 +0200

    add viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { NextComponentType, NextPageContext } from "next";
+import Head from "next/head";
 import type { NextRouter } from "next/router";
 
 import "tailwindcss/tailwind.css";
@@ -22,6 +23,12 @@ export default function App({
 }: AppRenderProps): JSX.Element {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
       <div className="bg-yellow-600 h-2" />
       <div className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-6xl xl:px-0">
         <Header />
