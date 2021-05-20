833de3410b8192f6670cee71301da2763cbfa312
Gerrit Alex <admin@gerritalex.de>
2021-05-21 00:40:21 +0200

    disable linting rule

--- packages/@keystone-heroes/web/.eslintrc.js
+++ packages/@keystone-heroes/web/.eslintrc.js
@@ -1,4 +1,21 @@
+const {
+  createConfig,
+  getDependencies,
+} = require("eslint-config-galex/src/createConfig");
+const {
+  createJestOverride,
+} = require("eslint-config-galex/src/overrides/jest");
+
+const deps = getDependencies();
+
+const jestOverride = createJestOverride({
+  ...deps,
+  rules: { "jest/require-top-level-describe": "off" },
+});
+
 module.exports = {
-  extends: "galex",
+  ...createConfig({
+    overrides: [jestOverride],
+  }),
   root: true,
 };
