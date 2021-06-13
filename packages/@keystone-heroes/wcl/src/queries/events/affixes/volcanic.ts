6499e19cc08ff056b2c72001083fb65e5cc7c6c3
Gerrit Alex <admin@gerritalex.de>
2021-06-14 01:30:21 +0200

    fix: reduce amount of volcanic events stored

--- packages/@keystone-heroes/wcl/src/queries/events/affixes/volcanic.ts
+++ packages/@keystone-heroes/wcl/src/queries/events/affixes/volcanic.ts
@@ -1,11 +1,19 @@
 import { EventDataType, HostilityType } from "../../../types";
 import type { DamageEvent } from "../types";
-import { createEventFetcher } from "../utils";
+import type { GetEventBaseParams } from "../utils";
+import { getEvents, reduceEventsByPlayer } from "../utils";
 
 export const VOLCANIC = 209_862;
 
-export const getVolcanicDamageTakenEvents = createEventFetcher<DamageEvent>({
-  dataType: EventDataType.DamageTaken,
-  hostilityType: HostilityType.Friendlies,
-  abilityID: VOLCANIC,
-});
+export const getVolcanicDamageTakenEvents = async (
+  params: GetEventBaseParams
+): Promise<DamageEvent[]> => {
+  const allEvents = await getEvents<DamageEvent>({
+    ...params,
+    dataType: EventDataType.DamageTaken,
+    hostilityType: HostilityType.Friendlies,
+    abilityID: VOLCANIC,
+  });
+
+  return reduceEventsByPlayer(allEvents, "targetID");
+};
