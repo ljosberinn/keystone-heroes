ee56e37efbba13183a054f6ac596bcdc1a2b472c
Gerrit Alex <admin@gerritalex.de>
2021-08-27 14:07:00 +0200

    feat: add pf doors

--- packages/@keystone-heroes/web/src/components/report/Map.tsx
+++ packages/@keystone-heroes/web/src/components/report/Map.tsx
@@ -530,6 +530,23 @@ const zoneDoors: Record<
   number,
   { type: DoorType; x: number; y: number; to: number }[]
 > = {
+  // Plaguefall
+  1674: [
+    {
+      type: "down",
+      x: 0.562_884_784_520_668_5,
+      y: 0.803_689_064_558_629_8,
+      to: 1697,
+    },
+  ],
+  1697: [
+    {
+      type: "up",
+      x: 0.536_499_560_246_262_1,
+      y: 0.424_242_424_242_424_25,
+      to: 1674,
+    },
+  ],
   // Theater of Pain
   1683: [
     {
