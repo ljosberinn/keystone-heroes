9642f2703eb7e4c915aa59dd4a57bec8ad3b70ee
Gerrit Alex <admin@gerritalex.de>
2021-10-26 14:09:32 +0200

    fix: handle failing img loads

--- packages/@keystone-heroes/web/src/components/AbilityIcon.tsx
+++ packages/@keystone-heroes/web/src/components/AbilityIcon.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type AbilityIconProps = {
   icon?: string | null;
   alt: string;
@@ -14,6 +16,37 @@ export const INVIS_POTION_ICON = `${STATIC_ICON_PREFIX}inv_alchemy_80_potion02or
 export const QUESTIONMARK_ICON = "inv_misc_questionmark";
 export const ZOOM_ICON = `${STATIC_ICON_PREFIX}inv_misc_spyglass_03.jpg`;
 
+const useIcon = (icon?: string | null) => {
+  const [errored, setErrored] = useState(false);
+  const [useWowhead, setUseWowhead] = useState(false);
+
+  const src =
+    !errored && useWowhead && icon
+      ? `https://wow.zamimg.com/images/wow/icons/medium/${icon}.jpg`
+      : `${STATIC_ICON_PREFIX}${
+          errored ? QUESTIONMARK_ICON : icon ?? QUESTIONMARK_ICON
+        }.jpg`;
+
+  return {
+    src,
+    onError: () => {
+      // if below fails.. run
+      if (errored) {
+        return;
+      }
+
+      // if wowhead fails aswell, fallback to questionmark
+      if (useWowhead) {
+        setErrored(true);
+        return;
+      }
+
+      // try wowhead fallback first
+      setUseWowhead(true);
+    },
+  };
+};
+
 export function AbilityIcon({
   icon,
   alt,
@@ -22,14 +55,18 @@ export function AbilityIcon({
   height,
   width,
 }: AbilityIconProps): JSX.Element {
+  const { src, onError } = useIcon(icon);
+
   return (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
     <img
-      src={`${STATIC_ICON_PREFIX}${icon ?? QUESTIONMARK_ICON}.jpg`}
+      src={src}
       alt={alt}
       title={title}
       className={className}
       width={width}
       height={height}
+      onError={onError}
     />
   );
 }
