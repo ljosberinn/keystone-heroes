cf1d7e70d64ca07ab5645f4febb527cc81ddf907
Gerrit Alex <admin@gerritalex.de>
2021-11-16 01:08:09 +0100

    chore: deps

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-
-import { IS_PRODUCTION } from "../web/env";
+import type { Prisma } from "@prisma/client";
 
 // add prisma to the NodeJS global type
 type CustomNodeJsGlobal = {
@@ -9,13 +8,18 @@ type CustomNodeJsGlobal = {
 
 declare const global: CustomNodeJsGlobal;
 
-if (typeof window !== "undefined") {
-  throw new TypeError("import error - do not bundle prisma to the client");
-}
-
 // Prevent multiple instances of Prisma Client in development
-export const prisma = global.prisma || new PrismaClient();
+// type casted to allow importing this file in the browser while also not
+// bundling prisma at all through treeshaking
+export const prisma =
+  typeof window === "undefined"
+    ? global.prisma || new PrismaClient()
+    : (null as unknown as PrismaClient<
+        Prisma.PrismaClientOptions,
+        never,
+        Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+      >);
 
-if (!IS_PRODUCTION) {
+if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
