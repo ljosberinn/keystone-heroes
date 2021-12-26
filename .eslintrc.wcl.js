c1ce6b8764bcc678d4c7c3a292cda937f2332a1f
Gerrit Alex <admin@gerritalex.de>
2021-12-26 19:03:48 +0100

    chore: linting

--- .eslintrc.wcl.js
+++ .eslintrc.wcl.js
@@ -25,6 +25,7 @@ const generatedTypesOverride = createTSOverride({
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "unicorn/no-keyword-prefix": "off",
+    "@typescript-eslint/no-unused-vars": "off",
   },
 });
 
