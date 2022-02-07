3080621cc4ca45cb8d17c72473f24eef16709344
Gerrit Alex <admin@gerritalex.de>
2022-02-07 21:46:06 +0100

    fix: drop debugger statement

--- src/api/functions/report.ts
+++ src/api/functions/report.ts
@@ -234,10 +234,6 @@ const createCharacterKey = (
 ) => `${character.name}-${character.serverID}`;
 
 const removeImageFormat = (icon: string) => {
-  if (!icon) {
-    debugger;
-  }
-
   if (!icon.includes(".")) {
     return icon;
   }
